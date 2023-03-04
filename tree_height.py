211d136bdbc98dc44c768d342fec4ed335349ecd
agaspars <82602542+agaspars@users.noreply.github.com>
2023-03-04 23:54:04 +0000

    Commit v1.0

--- tree_height.py
+++ tree_height.py
@@ -7,19 +7,41 @@ import threading
 def compute_height(n, parents):
     # Write this function
     max_height = 0
+    tree = [[] for _ in range(n)] #array of lists, each list contains children of each node(each element of array is a parent, containing list of children)
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+        else:
+            tree[parents[i]].append(i)
+
+    #recursion
+    def height(node):
+        if not tree[node]:#end of recursion - if node has no more children
+            return 1
+        return 1 + max(height(child) for child in tree[node])   #returns the max value of heights between children of a parent
+                                                                #return 1+child of a parent(going from root to each child using for loop)
+                                                                #if child exists => return 1+ child of a child until reach the end of tree
+
     # Your code here
+    max_height=height(root)
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    firstInput = input()
+    if "I" in firstInput:
+        
+        # let user input file name to use, don't allow file names with letter a
+        # account for github input inprecision
+        
+        # input number of elements
+        # input values in one variable, separate with space, split these values in an array
+        n = int(input())
+        parents = list(map(int, input().split()))
+        # call the function and output it's result
+        print(compute_height(n, parents))
+        pass
 
 
 # In Python, the default limit on recursion depth is rather low,
